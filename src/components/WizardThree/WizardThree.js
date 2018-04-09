af1799e50397f7e210a3a42632aca788e1f52ec7
ScottWorks <scottworks.io@gmail.com>
2018-04-09 17:10:31 -0600

    inital commit

--- src/components/WizardThree/WizardThree.js
+++ src/components/WizardThree/WizardThree.js
@@ -1,21 +1,51 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateProp } from '../../ducks/reducer';
 import { Link } from 'react-router-dom';
 
 class WizardThree extends Component {
-    render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">
-                   <p> What property are you looking to use the loan on? </p><br />
-                    <div className="row">
-                        <Link to="/wFour"><button value="primaryHome" onClick={this.props.propToBeUsedOn}>Primary Home</button></Link>
-                        <Link to="/wFour"><button value="rentalProperty" onClick={this.props.propToBeUsedOn}>Rental Property</button></Link>
-                        <Link to="/wFour"><button value="secondaryHome" onClick={this.props.propToBeUsedOn}>Secondary Home</button></Link>
-                    </div>
-                </div>           
-            </div>
-        )
-    }
+  render() {
+    const { updateProp } = this.props;
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p> What property are you looking to use the loan on? </p>
+          <br />
+          <div className="row">
+            <Link to="/wFour">
+              <button
+                value="primaryHome"
+                onClick={(event) => updateProp(event.target.value)}
+              >
+                Primary Home
+              </button>
+            </Link>
+            <Link to="/wFour">
+              <button
+                value="rentalProperty"
+                onClick={(event) => updateProp(event.target.value)}
+              >
+                Rental Property
+              </button>
+            </Link>
+            <Link to="/wFour">
+              onClick={(event) => updateProp(event.target.value)}
+              <button value="secondaryHome" onClick={this.props.propToBeUsedOn}>
+                Secondary Home
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default WizardThree; 
+function mapStateToProp(state) {
+  const { propToBeUsedOn } = state;
+  return {
+    propToBeUsedOn
+  };
+}
+
+export default connect(mapStateToProp, { updateProp })(WizardThree);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import store from './store';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById('root')
+);
